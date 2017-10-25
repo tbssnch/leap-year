9523901a02aedb2dce2d4ac842872e48e4f660a8
Sam Kissee and Tyler Bussanich <sam@>
2017-10-25 08:50:56 -0700

    add one additional passing spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
 
 // Business Logic
-  var leapYear = function(year) {
-    if (year % 100 === 0) {
-      return false;
-    } else if (year % 4 === 0) {
-      return true;
-    } else {
-      return false;
-    }
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
   // User Logic
